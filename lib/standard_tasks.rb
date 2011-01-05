b1d75c73605e8c7e1ab810825330de850409ceb5
Peter Hoeg <peter@hoeg.com>
2011-01-05 18:58:25 +0800

    fix: shuffled things around and make the structure a little simpler - hopefully this works...

--- lib/standard_tasks.rb
+++ lib/standard_tasks.rb
@@ -1,3 +1,16 @@
-if defined?(Rails)
-  require 'standard_tasks/railtie'
+module Northwind
+  if defined?(Rails)
+    class StandardTasks < Rails::Railtie
+      rake_tasks do
+	load "tasks/*.rake"
+      end
+#    initializer "newplugin.initialize" do |app|
+      # subscribe to all rails notifications: controllers, AR, etc.
+#      ActiveSupport::Notifications.subscribe do |*args|
+#        event = ActiveSupport::Notifications::Event.new(*args)
+#        puts "Got notification: #{event.inspect}"
+#      end
+#    end
+    end
+  end
 end
--- standard_tasks.gemspec
+++ standard_tasks.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{standard_tasks}
-  s.version = "0.0.4"
+  s.version = "0.0.5"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Peter Hoeg"]
@@ -25,7 +25,6 @@ Gem::Specification.new do |s|
     "Rakefile",
     "VERSION.yml",
     "lib/standard_tasks.rb",
-    "lib/standard_tasks/railtie.rb",
     "lib/tasks/clean.rake",
     "lib/tasks/db.rake",
     "lib/tasks/doc.rake",
