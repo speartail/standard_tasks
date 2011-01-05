63de8849d9855572f79b68a31e26197b3e4b57ea
Peter Hoeg <peter@hoeg.com>
2011-01-05 19:31:57 +0800

    why does it not load

--- lib/standard_tasks/railtie.rb
+++ lib/standard_tasks/railtie.rb
@@ -4,7 +4,9 @@ require 'rails'
 module StandardTasks
   class Railtie < Rails::Railtie
     rake_tasks do
-      Dir.glob('tasks/*.rake').each { |r| load r }
+      Dir.glob('../tasks/*.rake').each { |r| 
+        puts "peter: #{r}"
+        load r }
     end
   end
 end
